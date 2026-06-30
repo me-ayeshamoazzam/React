0ae2cacca063d06a6d32161753b6d8106be6c484
me-ayeshamoazzam <motiongraphicsbyayesha@gmail.com>
2026-06-30 16:59:49 +0500

    Made first component

---
+++ class-two/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+    return(
+        <>
+        <h1> Hello react </h1>  
+        <h2> This is first React component</h2>
+        </>
+
+    );
+}
+export default Home;
